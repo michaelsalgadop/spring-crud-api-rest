acacb7358b96ef5146b7a8e97f7d61a5415fe98f
Michaelsp11 <michaelsalgadop11@gmail.com>
2026-02-26 19:32:03 +0100

    feat: Realizamos un CRUD en JAVA que servimos a través de una API REST utilizando Spring Boot, Hibernate y PostgreSQL

---
+++ src/main/java/com/michaelsalgado/apirest/apirest/Entities/Producto.java
@@ -0,0 +1,34 @@
+package com.michaelsalgado.apirest.apirest.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
+    private double precio;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
---
+++ src/main/java/com/michaelsalgado/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.michaelsalgado.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.michaelsalgado.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+
+}
